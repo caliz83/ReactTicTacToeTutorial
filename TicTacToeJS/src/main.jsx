3964398c088533be52c33745fda44b7c63c7fca7
caliz83 <lizg1308@gmail.com>
2023-07-25 21:03:12 -0700

    first commit

---
+++ TicTacToeJS/src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
+//index.js === main.jsx
+
+// import { StrictMode } from "react";
+// import { createRoot } from 'react-dom/client';
+// import './style.css'
+
+// import App from "./App";
